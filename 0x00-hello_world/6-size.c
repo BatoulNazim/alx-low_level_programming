a52505319fef2a2f03ed183a4247bae415575e24
BatoulNazim <batoulnazim11@gmail.com>
2023-07-13 18:02:48 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
  * main - A program that prints the size of various computertypes
- * Return 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
-
 {
 	char a;
 	int b;
