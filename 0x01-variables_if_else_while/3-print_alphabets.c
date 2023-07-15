170f066353cf867c2f9373d81a8ce98c4c2c14af
BatoulNazim <batoulnazim11@gmail.com>
2023-07-14 17:30:48 -0700

    kk

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -14,9 +14,8 @@ int main(void)
 	for (low = 'a'; low <= 'z'; low++)
 	putchar(low);
 
-	for(low = 'A'; low <= 'Z';  low++)
+	for (low = 'A'; low <= 'Z';  low++)
 	putchar(low);
-	putchar('\n');\
-	
+	putchar('\n');
 	return (0);
 }
