7f02cdfe1d34d96a1e6bc173b1d34957110ccc04
BatoulNazim <batoulnazim11@gmail.com>
2023-07-14 18:01:34 -0700

    kk

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,11 +12,12 @@ int main(void)
 	char low, e, q;
 	e = 'e';
 	q = 'q';
+
 	for (low = 'a'; low <= 'z'; low++)
 	{
 	if (low != e && low != q)
 	putchar(low);
 	}
-	putchar('n');
+	putchar('\n');
 	return (0);
 }
