6eb7642b6a68c394197cb2b7ca792483582481fb
BatoulNazim <batoulnazim11@gmail.com>
2023-07-18 18:44:39 -0700

    k

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void times_table(void) {
+  int i, j;
+  for (i = 0; i <= 9; i++) {
+    for (j = 0; j <= 9; j++) {
+      if (j == 0) {
+        printf("%d ", i);
+      } else {
+        printf("%d ", i * j);
+      }
+    }
+    printf("\n");
+  }
+}
