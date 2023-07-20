0214b7984a52aa1ea878e60d0a16522b99eb6cd3
BatoulNazim <batoulnazim11@gmail.com>
2023-07-20 16:18:13 -0700

    ll

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,7 +4,7 @@
  * nul - Multiplies two integers
  * @x: The first number to multiply
  * @y: The second number to multiply with first number
- * Return: The result of multiplication between x param and y param
+ * Return: The result of multiplication between x paran and y paran
  */
 int nul(int x, int y)
 {
