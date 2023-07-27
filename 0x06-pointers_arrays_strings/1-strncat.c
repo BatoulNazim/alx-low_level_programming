0069e83eb2af6087833e70c64a0ab70e22e59739
BatoulNazim <batoulnazim11@gmail.com>
2023-07-27 03:59:16 -0700

    lol

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,16 +10,21 @@
 char *_strcat(char *dest, char *src, int n)
 {
 int i;
+int j;
 
-for (i = 0; i < n && src[i] != '\0'; i++)
+i = 0;
+while I(dest[i] != '\0')
 {
-dest[i] = src[i];
+i++;
+}
+j = 0;
+while (j < n && src[j] != '\0')
+{
+dest[i] = src[j]
+i++;
+j++;
 }
 
-if (i < n)
-{
 dest[i] = '\0';
-}
-
 return (dest);
 }
