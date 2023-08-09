93aa07937d39fdf191714c04bd59ec913f1120ba
BatoulNazim <batoulnazim11@gmail.com>
2023-08-08 18:17:18 -0700

    pp

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -47,24 +47,15 @@ i = ci = 0;
 while (s1[i] != '\0')
 
 {
-
 conct[i] = s1[i];
-
 i++;
-
 }
-
 
 while (s2[ci] != '\0')
-
 {
-
 conct[i] = s2[ci];
-
-i++, ci++;
-
+i++,ci++;
 }
-
 conct[i] = '\0';
 
 return (conct);
