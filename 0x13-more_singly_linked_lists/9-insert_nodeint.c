ae82f2cd7afcee75f52e021944c800af42664bf3
BatoulNazim <batoulnazim11@gmail.com>
2023-08-28 11:29:08 -0700

    ll

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 
+
 /**
  * insert_nodeint_at_index - inserts a new node in a linked list,
  * at a given position
@@ -64,5 +65,10 @@ return (new);
 else
 
 temp = temp->next;
+
+}
+
+
+return (NULL);
 
 }
